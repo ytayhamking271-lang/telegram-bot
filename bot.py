4fc78a365f7d1388ba59d64c5892ec654a2d117d
ytayhamking271-lang <ytayhamking271@gmail.com>
2026-06-01 03:11:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,16 +4,31 @@ from telegram import Bot
 
 TOKEN = os.getenv("TOKEN")
 
-GROUP_ID = -1001719475141
+GROUP_IDS = [
+    -1001719475141,
+    -1002318437904,
+    -1002450763132,
+    -1003740539352,
+    -1002630229848,
+    -1003919199766,
+    -1003729495750,
+    -1003520088160,
+    -1003126524114,
+    -1001755122717
+]
 
 async def main():
     bot = Bot(TOKEN)
 
-    msg = await bot.send_message(
-        chat_id=GROUP_ID,
-        text="Test från Railway"
-    )
+    for gid in GROUP_IDS:
+        try:
+            msg = await bot.send_message(
+                chat_id=gid,
+                text="Test"
+            )
+            print(f"OK: {gid}")
 
-    print(msg)
+        except Exception as e:
+            print(f"FEL: {gid} -> {e}")
 
 asyncio.run(main())
